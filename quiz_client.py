56a3fc25dae29273fd13ce53a4a579721238f876
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-06-25 11:06:17 +0530

    Add files via upload

--- quiz_client.py
+++ quiz_client.py
@@ -2,71 +2,181 @@ import socket
 from threading import Thread
 from tkinter import *
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-nickname=input("Enter your nickname: ")
-client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip_address = '127.0.0.1'
+port = 8000
 
-IPAddress = "127.0.0.1"
-port=8001
+client.connect((ip_address, port))
 
-client.connect((IPAddress, port))
 print("Connected with the server...")
 
-class GUI():
-    def __init__(self):
-        self.window = Tk()
-        self.window.withdraw()
+class GUI:
+	def __init__(self):
+		
+		self.Window = Tk()
+		self.Window.withdraw()
+		
+		self.login = Toplevel()
+		self.login.title("Login")
+		self.login.resizable(width = False,
+							height = False)
+		self.login.configure(width = 400,
+							height = 300)
+		self.pls = Label(self.login,
+					text = "Please login to continue",
+					justify = CENTER,
+					font = "Helvetica 14 bold")
+		
+		self.pls.place(relheight = 0.15,
+					relx = 0.2,
+					rely = 0.07)
+		self.labelName = Label(self.login,
+							text = "Name: ",
+							font = "Helvetica 12")
+		
+		self.labelName.place(relheight = 0.2,
+							relx = 0.1,
+							rely = 0.2)
+		
+		self.entryName = Entry(self.login,
+							font = "Helvetica 14")
+		
+		self.entryName.place(relwidth = 0.4,
+							relheight = 0.12,
+							relx = 0.35,
+							rely = 0.2)
+		
+		self.entryName.focus()
 
-        self.login = Toplevel()
-        self.login.title("Login")
-        self.login.resizable(width=False, height=False)
-        self.login.configure(width=400, height=300)
+		self.go = Button(self.login,
+						text = "CONTINUE",
+						font = "Helvetica 14 bold",
+						command = lambda: self.goAhead(self.entryName.get()))
+		
+		self.go.place(relx = 0.4,
+					rely = 0.55)
+		self.Window.mainloop()
 
-        self.pls = Label(self.login, text="Please login to continue", justify="center", font="Helvetica 14 bold")
-        self.pls.place(relheight=0.15, relx=0.2, rely=0.07)
+	def goAhead(self, name):
+		self.login.destroy()
+		self.layout(name)
+		rcv = Thread(target=self.receive)
+		rcv.start()
 
-        self.labelname = Label(self.login, text="Name", font="Helvetica 12")
-        self.labelname.place(relheight=0.2, relx=0.1, rely=0.2)
+	def layout(self,name):
+		
+		self.name = name
+		self.Window.deiconify()
+		self.Window.title("CHATROOM")
+		self.Window.resizable(width = False,
+							height = False)
+		self.Window.configure(width = 470,
+							height = 550,
+							bg = "#17202A")
+		self.labelHead = Label(self.Window,
+							bg = "#17202A",
+							fg = "#EAECEE",
+							text = self.name ,
+							font = "Helvetica 13 bold",
+							pady = 5)
+		
+		self.labelHead.place(relwidth = 1)
+		self.line = Label(self.Window,
+						width = 450,
+						bg = "#ABB2B9")
+		
+		self.line.place(relwidth = 1,
+						rely = 0.07,
+						relheight = 0.012)
+		
+		self.textCons = Text(self.Window,
+							width = 20,
+							height = 2,
+							bg = "#17202A",
+							fg = "#EAECEE",
+							font = "Helvetica 14",
+							padx = 5,
+							pady = 5)
+		
+		self.textCons.place(relheight = 0.745,
+							relwidth = 1,
+							rely = 0.08)
+		
+		self.labelBottom = Label(self.Window,
+								bg = "#ABB2B9",
+								height = 80)
+		
+		self.labelBottom.place(relwidth = 1,
+							rely = 0.825)
+		
+		self.entryMsg = Entry(self.labelBottom,
+							bg = "#2C3E50",
+							fg = "#EAECEE",
+							font = "Helvetica 13")
+		
+		self.entryMsg.place(relwidth = 0.74,
+							relheight = 0.06,
+							rely = 0.008,
+							relx = 0.011)
+		
+		self.entryMsg.focus()
+		
+		self.buttonMsg = Button(self.labelBottom,
+								text = "Send",
+								font = "Helvetica 10 bold",
+								width = 20,
+								bg = "#ABB2B9",
+								command = lambda: self.sendButton(self.entryMsg.get()))
+		
+		self.buttonMsg.place(relx = 0.77,
+							rely = 0.008,
+							relheight = 0.06,
+							relwidth = 0.22)
+		
+		self.textCons.config(cursor = "arrow")
+		
+		scrollbar = Scrollbar(self.textCons)
 
-        self.entry = Entry(self.login, font="Helvetica 14")
-        self.entry.place(relheight=0.12, relwidth=0.4, relx=0.35, rely=0.2)
-        self.entry.focus()
+		scrollbar.place(relheight = 1,
+						relx = 0.974)
+		
+		scrollbar.config(command = self.textCons.yview)
+		
+		self.textCons.config(state = DISABLED)
 
-        self.go = Button(self.login, text="Continue", font="Helvetica 14 bold", command=lambda:self.goahed(self.entry.get()))
-        self.go.place(relx=0.4, rely=0.5)
-        
-        self.window.mainloop()
+	def sendButton(self, msg):
+		self.textCons.config(state = DISABLED)
+		self.msg=msg
+		self.entryMsg.delete(0, END)
+		snd= Thread(target = self.write)
+		snd.start()
 
-    def goahed(self, name):
-        self.login.destroy()
-        self.name=name
-        rcv=Thread(target=self.recieve)
-        rcv.start()
+	def show_message(self, message):
+		self.textCons.config(state = NORMAL)
+		self.textCons.insert(END, message+"\n\n")
+		self.textCons.config(state = DISABLED)
+		self.textCons.see(END)
 
-    def recieve(self):
-        while True:
-            try:
-                message=client.recv(2048).decode("UTF-8")
-                if message == "NICKNAME":
-                    client.send(nickname.encode("utf-8"))
-                else:
-                    print(message)
-            except:
-                print("An error occurred")
-                client.close()
-                break
+	def receive(self):
+		while True:
+			try:
+				message = client.recv(2048).decode('utf-8')
+				if message == 'NICKNAME':
+					client.send(self.name.encode('utf-8'))
+				else:
+					self.show_message(message)
+			except:
+				print("An error occured!")
+				client.close()
+				break
 
-objectname = GUI()
+	def write(self):
+		self.textCons.config(state=DISABLED)
+		while True:
+			message = (f"{self.name}: {self.msg}")
+			client.send(message.encode('utf-8'))
+			self.show_message(message)	
+			break
 
-'''def write():
-    while True:
-        message="{}: {}".format(nickname, input(""))
-        client.send(message.encode("utf-8"))
-
-recieveThread=Thread(target=recieve)
-recieveThread.start()
-
-writeThread=Thread(target=write)
-writeThread.start()'''
-
- 
+g = GUI()
--- quiz_server.py
+++ quiz_server.py
@@ -2,47 +2,53 @@ import socket
 from threading import Thread
 import random
 
-server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IPAddress = "127.0.0.1"
-port=8001
-server.bind((IPAddress, port))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+port = 8000
+
+server.bind((ip_address, port))
 server.listen()
-listOfClients=[]
-nicknames=[]
 
-print("Server is started!")
+list_of_clients = []
+nicknames = []
 
-question={
-    "What is the biggest megacity of India? \n a.Bengaluru\n b.Delhi\n c.Kolkata\n d.Mumbai",
-    "Who invented Crescograph? \n a.Einstein\n b.Satyendranath Bose\n c.Jagadish Chandra Bose\n d.Charles Darwin",
-    "What is the largest National Highway of India? \n a.NH12\n b.NH36\n c.NH44\n d.NH64",
-    "On whose birthday we celebrate National Youth Day? \n a.Netaji Subhash Chandra Bose\n b.Bhagat Singh\n c.Shivaji\n d.Swami Vivekananda"
-    
-}
+questions = [
+     " What is the Italian word for PIE? \n a.Mozarella\n b.Pasty\n c.Patty\n d.Pizza",
+     " Water boils at 212 Units at which scale? \n a.Fahrenheit\n b.Celsius\n c.Rankine\n d.Kelvin",
+     " Which sea creature has three hearts? \n a.Dolphin\n b.Octopus\n c.Walrus\n d.Seal",
+     " Who was the character famous in our childhood rhymes associated with a lamb? \n a.Mary\n b.Jack\n c.Johnny\n d.Mukesh",
+     " How many bones does an adult human have? \n a.206\n b.208\n c.201\n d.196",
+     " How many wonders are there in the world? \n a.7\n b.8\n c.10\n d.4",
+     " What element does not exist? \n a.Xf\n b.Re\n c.Si\n d.Pa",
+     " How many states are there in India? \n a.24\n b.29\n c.30\n d.31",
+     " Who invented the telephone? \n a.A.G Bell\n b.John Wick\n c.Thomas Edison\n d.G Marconi",
+     " Who is Loki? \n a.God of Thunder\n b.God of Dwarves\n c.God of Mischief\n d.God of Gods",
+     " Who was the first Indian female astronaut ? \n a.Sunita Williams\n b.Kalpana Chawla\n c.None of them\n d.Both of them ",
+     " What is the smallest continent? \n a.Asia\n b.Antarctic\n c.Africa\n d.Australia",
+     " The beaver is the national embelem of which country? \n a.Zimbabwe\n b.Iceland\n c.Argentina\n d.Canada",
+     " How many players are on the field in baseball? \n a.6\n b.7\n c.9\n d.8",
+     " Hg stands for? \n a.Mercury\n b.Hulgerium\n c.Argenine\n d.Halfnium",
+     " Who gifted the Statue of Libery to the US? \n a.Brazil\n b.France\n c.Wales\n d.Germany",
+     " Which planet is closest to the sun? \n a.Mercury\n b.Pluto\n c.Earth\n d.Venus"
+]
 
-answer={"d","c","c","d"}
+answers = ['d', 'a', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 'c', 'b', 'd', 'd', 'c', 'a', 'b', 'a']
+
+print("Server has started...")
 
 def get_random_question_answer(conn):
-  random_index = random.randint(0,len(question)-1)
-  random_question = question[random_index]
-  random_answer=answer[random_index]
-  conn.send(random_question.encode("UTF-8"))
-  return random_index, random_question, random_answer 
+    random_index = random.randint(0,len(questions) - 1)
+    random_question = questions[random_index]
+    random_answer = answers[random_index]
+    conn.send(random_question.encode('utf-8'))
+    return random_index, random_question, random_answer
 
 def remove_question(index):
-  question.pop(index)
-  answer.pop(index)
+    questions.pop(index)
+    answers.pop(index)
 
-
-def remove(conn):
-  if conn in listOfClients:
-    listOfClients.remove(conn)
-
-def remove_nickname(nickname):
-  if nickname in nicknames:
-    nicknames.remove(nickname)
-
-def clientThread(conn, nickname):
+def clientthread(conn, nickname):
     score = 0
     conn.send("Welcome to this quiz game!".encode('utf-8'))
     conn.send("You will receive a question. The answer to that question should be one of a, b, c or d!\n".encode('utf-8'))
@@ -68,12 +74,20 @@ def clientThread(conn, nickname):
             print(str(e))
             continue
 
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+
+def remove_nickname(nickname):
+    if nickname in nicknames:
+        nicknames.remove(nickname)
+
 while True:
-    conn, address = server.accept()
-    conn.send('NICKNAME'.encode("UTF-8"))
-    nickname = conn.recv(2048).decode("UTF-8")
-    listOfClients.append(conn)
+    conn, addr = server.accept()
+    conn.send('NICKNAME'.encode('utf-8'))
+    nickname = conn.recv(2048).decode('utf-8')
+    list_of_clients.append(conn)
     nicknames.append(nickname)
-    print(nickname + "connected!")
-    newThread = Thread(target=clientThread, args=(conn, nickname))
-    newThread.start()
+    print (nickname + " connected!")
+    new_thread = Thread(target= clientthread,args=(conn,nickname))
+    new_thread.start()
